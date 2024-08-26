b3c024423b4c32cf491a82e15b26a721e7d00d58
shahbozshirinboyev <106464818+shahbozshirinboyev@users.noreply.github.com>
2024-08-26 17:12:52 +0500

    Create pages Home About Contact and Connect react router dom

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+
+// react-router-dom
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <BrowserRouter>
+
+        <nav>
+          <h1>My artikles</h1>
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga, tempore, natus odit labore iure laudantium nisi tenetur animi debitis modi consequatur reprehenderit aliquid saepe? Ipsum eius corrupti minima placeat. Ab non eligendi doloremque commodi delectus facilis cum repellat! Consequatur eaque quam ea eius quaerat reprehenderit nisi! Aperiam fugiat sapiente accusantium.</p>
+    </div>
+  )
+}
+
+export default About
