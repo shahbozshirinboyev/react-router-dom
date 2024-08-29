8bc4b53bda1362e6a393a84a8cc9547833d1947e
shahbozshirinboyev <shahboz.sh.b@gmail.com>
2024-08-29 08:15:10 +0500

    End Article detail

--- src/pages/articles/ArticlesDetasil.jsx
+++ src/pages/articles/ArticlesDetasil.jsx
@@ -1,28 +1,27 @@
-import { useParams } from "react-router-dom"
-import { useFetch } from "../../hooks/useFetch"
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
 
 function ArticlesDetasil() {
-
-  const { id } = useParams()
-  const url = "http://localhost:5173/articles/" + id
-  const  {data: article, error, isPending} = useFetch(url)
+  const { id } = useParams();
+  const url = "http://localhost:3000/articles/" + id;
+  const { data: article, error, isPending } = useFetch(url);
 
   return (
     <div>
-      {isPending && <h3>Loading ...</h3> }
-      {error && <h3>{error}</h3> }
-      {article && <>
-        <img src={article.image} alt={article.title} height={300} />
-        <h2>Title: {article.title}</h2>
-        <p>Author: <i>{article.author}</i></p>
-        <p>
-          {article.body}
-        </p>
-      </>
-      }
-
+      {isPending && <h3>Loading ...</h3>}
+      {error && <h3>{error}</h3>}
+      {article && (
+        <>
+          <img src={article.image} alt={article.title} height={300} />
+          <h2>Title: {article.title}</h2>
+          <p>
+            Author: <i>{article.author}</i>
+          </p>
+          <p>{article.body}</p>
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default ArticlesDetasil
+export default ArticlesDetasil;
